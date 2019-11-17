e39af43ef4c751931501fdd08b23372300f7fb9c
TShibano <46261399+TShibano@users.noreply.github.com>
2019-11-17 18:15:13 +0900

    OK

--- AtCoder_Python/folder_file_create.py
+++ AtCoder_Python/folder_file_create.py
@@ -8,15 +8,15 @@ def save_file_at_new_dir(new_dir_path, new_filename, new_file_content, mode="w")
 def create(start, end, S):      # S:list
     for i in range(start, end + 1):
         for s in S:
-            new_dir_path = "ABC0" + str(i)
-            new_filename = "ABC_0" + str(i) + "_" + s + ".py"
-            new_file_content = "ABC" + str(i) + s
+            new_dir_path = "ARC" + str(i).zfill(3)
+            new_filename = "ARC_" + str(i).zfill(3) + "_" + s + ".cpp"
+            new_file_content = "// ARC" + str(i).zfill(3) + s
             save_file_at_new_dir(new_dir_path, new_filename, new_file_content)
             
 
 def main():
-    start = 42
-    end = 99
+    start = 1
+    end = 103
     S = ["A", "B", "C", "D"]
     create(start, end, S)
 
