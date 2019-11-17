8ba87772aff0079ba3c282caa24290f47548c465
TShibano <46261399+TShibano@users.noreply.github.com>
2019-11-17 17:07:21 +0900

    OK

--- AtCoder_Python/ABC141/ABC_141_A.py
+++ AtCoder_Python/ABC141/ABC_141_A.py
@@ -1,7 +1,1 @@
 # ABC141A
-N =  int(input())
-if N % 2 == 0:
-    ans = (N // 2) / N
-else:
-    ans = ((N + 1) // 2) / N
-print("{:.10f}".format(ans))
--- AtCoder_Python/ABC141/ABC_141_B.py
+++ AtCoder_Python/ABC141/ABC_141_B.py
@@ -1,8 +1,1 @@
 # ABC141B
-N, K = map(int, input().split())
-H = list(map(int, input().split()))
-ans = 0
-for h in H:
-    if h >= K:
-        ans += 1
-print(ans)
