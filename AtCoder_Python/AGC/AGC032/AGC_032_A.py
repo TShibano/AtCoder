0bfecc47e11646e86d7ee4f59769e9f25dfa6703
TShibano <46261399+TShibano@users.noreply.github.com>
2019-11-18 15:28:55 +0900

    2019/11/18

--- AtCoder_Python/AGC/AGC032/AGC_032_A.py
+++ AtCoder_Python/AGC/AGC032/AGC_032_A.py
@@ -1,1 +1,16 @@
-# AGC032A
+# AGC032A-解説見た
+# 逆を考えて右端からB[k] = k+1が一致したところを取り出していく
+N = int(input())
+B = list(map(int, input().split()))
+ans_list = []
+for i in range(N):
+    for k in range(len(B)-1, -1, -1):
+        if B[k] == k+1:
+            ans_list.append(B[k])
+            del B[k]
+            break
+        if k == 0:
+            print(-1)
+            exit()
+for k in range(len(ans_list)):
+    print(ans_list[- 1 - k])
