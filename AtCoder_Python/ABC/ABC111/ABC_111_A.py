7f71ce8e2ed24788fc591da3555eeb063a3096ad
TShibano <46261399+TShibano@users.noreply.github.com>
2020-01-05 23:37:44 +0900

    2020/01/05

--- AtCoder_Python/ABC/ABC111/ABC_111_A.py
+++ AtCoder_Python/ABC/ABC111/ABC_111_A.py
@@ -1,1 +1,8 @@
 # ABC111A
+n = str(input())
+
+n = n.replace('1', '8')
+n = n.replace('9', '2')
+n = n.replace('8', '9')
+n = n.replace('2', '1')
+print(n)
--- AtCoder_Python/ABC/ABC111/ABC_111_C.py
+++ AtCoder_Python/ABC/ABC111/ABC_111_C.py
@@ -1,1 +1,43 @@
 # ABC111C
+# ABC111C
+import collections
+
+n = int(input())
+v = list(map(int, input().split()))
+if len(set(v)) == 1:
+    print(n//2)
+    exit()
+
+v_odd = [0] * (n // 2)
+v_even = [0] * (n // 2)
+for i in range(n):
+    if (i+1)%2 == 1:
+        v_odd[i//2] = v[i]
+    else:
+        v_even[i//2] = v[i]
+c_v_odd = collections.Counter(v_odd)
+c_v_even = collections.Counter(v_even)
+
+if c_v_odd.most_common()[0][0] == c_v_even.most_common()[0][0]:
+    if c_v_odd.most_common()[0][1] > c_v_even.most_common()[0][1]:
+        odd_count = n//2 - c_v_odd.most_common()[0][1]
+        even_count = n//2 - c_v_even.most_common()[1][1]
+        ans = odd_count + even_count
+    elif c_v_odd.most_common()[0][1] < c_v_even.most_common()[0][1]:
+        odd_count = n//2 - c_v_odd.most_common()[1][1]
+        even_count = n//2 - c_v_even.most_common()[0][1]
+        ans = odd_count + even_count
+    else:
+        if c_v_odd.most_common()[1][1] > c_v_even.most_common()[1][1]:
+            odd_count = n//2 - c_v_odd.most_common()[1][1]
+            even_count = n//2 - c_v_even.most_common()[0][1]
+            ans = odd_count + even_count
+        else:
+            odd_count = n//2 - c_v_odd.most_common()[0][1]
+            even_count = n//2 - c_v_even.most_common()[1][1]
+            ans = odd_count + even_count
+else:
+    odd_count = n//2 -  c_v_odd.most_common()[0][1]
+    even_count = n//2 -  c_v_even.most_common()[0][1]
+    ans = odd_count + even_count
+print(ans)
