287305effd40f8c06561113120e92fa6e9f8014e
TShibano <46261399+TShibano@users.noreply.github.com>
2020-01-10 16:28:01 +0900

    2020/01/10

--- AtCoder_Python/ABC/ABC052/ABC_052_A.py
+++ AtCoder_Python/ABC/ABC052/ABC_052_A.py
@@ -1,1 +1,6 @@
 # ABC052A
+A, op, B = map(str, input().split())
+if op == "+":
+    print(int(A) + int(B))
+else:
+    print(int(A) - int(B))
--- AtCoder_Python/ABC/ABC052/ABC_052_B.py
+++ AtCoder_Python/ABC/ABC052/ABC_052_B.py
@@ -1,1 +1,12 @@
 # ABC052B
+N = int(input())
+S = input()
+x_lst = [0]
+x = 0
+for i in range(N):
+    if S[i] == "I":
+        x += 1
+    else:
+        x -= 1
+    x_lst.append(x)
+print(max(x_lst))
