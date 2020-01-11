626ca5befae212cdf453e4955a14934c72e60d44
TShibano <46261399+TShibano@users.noreply.github.com>
2020-01-11 18:45:52 +0900

    2020/01/11

--- AtCoder_Python/ABC/ABC077/ABC_077_B.py
+++ AtCoder_Python/ABC/ABC077/ABC_077_B.py
@@ -1,1 +1,19 @@
 # ABC077B
+import math
+def check(N):
+    n = math.sqrt(N)
+    return math.floor(n) == math.ceil(n)
+N = int(input())
+flag = False
+for i in range(N, 0, -1):
+    flag = check(i)
+    if flag:
+        print(i)
+        break
+        
+# 解説
+N = int(input())
+for i in range(1, N+2):
+    if i ** 2 > N:
+        print((i - 1) ** 2)
+        break
--- AtCoder_Python/ABC/ABC077/ABC_077_C.py
+++ AtCoder_Python/ABC/ABC077/ABC_077_C.py
@@ -1,1 +1,32 @@
 # ABC077C
+import bisect
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+A.sort()
+B.sort()
+C.sort()
+ans = 0
+for i in range(N):
+    b_min = bisect.bisect_right(B, A[i])
+    for k in range(b_min, N):
+        c_min = bisect.bisect_right(C, B[k])
+        ans += N - c_min
+        ##print(i, k, ans)
+
+print(ans)
+
+# 以下，解説をみた
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+A.sort()
+C.sort()
+ans = 0
+for b in B:
+    a_max = bisect.bisect_left(A, b)
+    c_min = bisect.bisect_right(C, b)
+    ans += a_max * (N - c_min)
+print(ans)
