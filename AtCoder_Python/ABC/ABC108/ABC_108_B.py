7064d7ea18b795d0862b36d98bd0c54c97fb93dd
TShibano <46261399+TShibano@users.noreply.github.com>
2020-01-16 14:21:28 +0900

    2020/01/16

--- AtCoder_Python/ABC/ABC108/ABC_108_B.py
+++ AtCoder_Python/ABC/ABC108/ABC_108_B.py
@@ -1,1 +1,27 @@
 # ABC108B
+a, b, c, d = map(int, input().split())
+# (x1, y1)を原点に移動させる
+c -= a
+d -= b
+if c > 0 and d >= 0:     # 第一象限
+    e = c - d
+    f = c + d
+    g = -abs(d)
+    h = abs(c)
+elif c <= 0 and d > 0:       # 第二象限
+    e = c - abs(d)
+    f = d - abs(c)
+    g = -abs(d)
+    h = -abs(c)
+elif c < 0 and d <= 0:       # 第三象限
+    e = c + abs(d)
+    f = d - abs(c)
+    g = abs(d)
+    h = -abs(c)
+else:       # 第四象限
+    e = c + abs(d)
+    f = d + abs(c)
+    g = abs(d)
+    h = abs(c)
+
+print("{} {} {} {}".format(e+a , f+b , g+a ,h+b))
--- AtCoder_Python/ABC/ABC108/ABC_108_C.py
+++ AtCoder_Python/ABC/ABC108/ABC_108_C.py
@@ -1,1 +1,28 @@
 # ABC108C
+N, K = map(int, input().split())
+
+ans = 0
+
+if N == 1 and K != 1:
+    print(0)
+    exit()
+if K % 2 == 0:
+    # a, b, c が全て K の倍数
+    cnt = N // K 
+    ans = cnt ** 3
+    K //= 2
+    k = K
+    # a, b, c が K//2 の奇数倍
+    tmp = 1
+    while 1:
+        K += 2 * k
+        if N < K:
+            break
+        tmp += 1
+    ##print(cnt, tmp)
+    ans += (tmp) ** 3
+else:
+    cnt = N // K
+    ans = cnt ** 3
+
+print(ans)
