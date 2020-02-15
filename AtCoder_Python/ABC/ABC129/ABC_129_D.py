102aefaf12a598f0291d9e6a82f7d3c89c8faeb8
TShibano <46261399+TShibano@users.noreply.github.com>
2020-02-15 18:18:45 +0900

    2020/02/15

--- AtCoder_Python/ABC/ABC129/ABC_129_D.py
+++ AtCoder_Python/ABC/ABC129/ABC_129_D.py
@@ -1,1 +1,55 @@
 # ABC129D
+H, W = map(int, input().split())
+field = [input() for _ in range(H)]
+
+# Left
+Left = [[0] * W for _ in range(H)]
+for h in range(H):
+    cnt = 0
+    for w in range(W):
+        if field[h][w] == '#':
+            cnt = 0
+        else:
+            cnt += 1
+        Left[h][w] = cnt
+
+# Right
+Right = [[0] * W for _ in range(H)]
+for h in range(H):
+    cnt = 0
+    for w in range(W-1, -1, -1):
+        if field[h][w] == '#':
+            cnt = 0
+        else:
+            cnt += 1
+        Right[h][w] = cnt
+
+# Up
+Up = [[0] * W for _ in range(H)]
+for w in range(W):
+    cnt = 0
+    for h in range(H):
+        if field[h][w] == '#':
+            cnt = 0
+        else:
+            cnt += 1
+        Up[h][w] = cnt
+
+# Down
+Down = [[0] * W for _ in range(H)]
+for w in range(W):
+    cnt = 0
+    for h in range(H-1, -1, -1):
+        if field[h][w] == '#':
+            cnt = 0
+        else:
+            cnt += 1
+        Down[h][w] = cnt
+
+# 集計
+ans = -1
+for h in range(H):
+    for w in range(W):
+        ans = max(ans, Left[h][w] + Right[h][w] + Up[h][w] + Down[h][w] - 3)
+print(ans)
+
