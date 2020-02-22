7ffe3b09e0b33f7cf35dca7ac356b035dd40aaa7
TShibano <46261399+TShibano@users.noreply.github.com>
2020-02-22 14:07:09 +0900

    2020/02/18

--- AtCoder_Python/AGC/AGC022/AGC_022_A.py
+++ AtCoder_Python/AGC/AGC022/AGC_022_A.py
@@ -1,1 +1,38 @@
 # AGC022A
+S = input()
+al=[chr(ord('a') + i) for i in range(26)]
+
+if S == "zyxwvutsrqponmlkjihgfedcba":
+    print(-1)
+    exit()
+
+if len(S) != 26:
+    check =[[0] * 2 for _ in range(26)]
+    for i in range(len(S)):
+        for a in range(26):
+            if S[i] == al[a]:
+                check[a][0] = 1
+                check[a][1] = i
+    for i in range(26):
+        if check[i][0] == 0:
+            used_al = al[i]
+            break
+    print(S + used_al)
+    exit()
+ans = ""
+lst = []
+for i in range(24, -1, -1):
+    if S[i] > S[i+1]:
+        lst.append(S[i+1])
+    else:
+        lst.append(S[i+1])
+        tmp = S[i]
+        lst.append(S[i])
+        ans += S[:i]
+        lst.sort()
+        for j in range(len(lst)):
+            if tmp == lst[j]:
+                ans += lst[j+1]
+                break
+        print(ans)
+        exit()
--- AtCoder_Python/AGC/AGC039/AGC_039_A.py
+++ AtCoder_Python/AGC/AGC039/AGC_039_A.py
@@ -1,1 +1,38 @@
 # AGC039A
+def RanLengthEncoding(S):
+    N = len(S)
+    RLE = []
+    now_char = S[0]
+    now_cnt = 1
+    for i in range(1, N):
+        if now_char == S[i]:
+            now_cnt += 1
+        else:
+            RLE.append([now_char, now_cnt])
+            now_char = S[i]
+            now_cnt = 1
+    RLE.append([now_char, now_cnt])
+    return RLE
+
+S = input()
+K = int(input())
+rle = RanLengthEncoding(S)
+if len(rle) == 1:
+    ans = rle[0][1] * K // 2
+    print(ans)
+    exit()
+cnt = 0
+for i in range(1, len(rle)-1):
+    cnt += rle[i][1] // 2
+
+if rle[0][0] != rle[-1][0]:
+    cnt += rle[0][1] // 2
+    cnt += rle[-1][1] // 2
+    ans = cnt * K
+else:
+    tmp = (rle[0][1] + rle[-1][1]) // 2
+    ans = cnt * K
+    ans += tmp * (K-1)
+    ans += rle[0][1] // 2
+    ans += rle[-1][1] // 2
+print(ans)
